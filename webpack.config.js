7af9b383fd378a15b7bd607b9024e4573be3e20c
eddie naff <eddienaff@eddienaff.com>
2020-08-26 14:03:57 -0700

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: {
+    index: "./src/webapp/frontend/index.tsx",
+    actions: "./src/webapp/frontend/actions.tsx",
+    upload: "./src/webapp/frontend/upload.tsx",
+  },
+  output: {
+    path: __dirname + "/public",
+    filename: "build/[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
+  },
+  devServer: {
+    contentBase: __dirname + "/public",
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:9000",
+      },
+      "/auth": {
+        target: "http://localhost:9000",
+      },
+    },
+  },
+};
